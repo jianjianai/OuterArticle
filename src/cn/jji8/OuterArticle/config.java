f37862417efff51e53edeb00a191cb674bea506b
简简爱 <59829816+jianjianai@users.noreply.github.com>
2020-08-07 15:51:48 +0800

    添加了名字的筛选按

---
+++ src/cn/jji8/OuterArticle/config.java
@@ -0,0 +1,8 @@
+package cn.jji8.OuterArticle;
+
+public class config {
+    config(main main){
+        System.out.println(main.getConfig().getMapList("点击命令"));
+
+    }
+}
--- src/cn/jji8/OuterArticle/main.java
+++ src/cn/jji8/OuterArticle/main.java
@@ -12,36 +12,12 @@ import java.io.File;
 import java.io.IOException;
 
 public class main extends JavaPlugin implements Listener {
+    config config;
     public void onEnable(){
         getLogger().info("开始加载..");
+        saveDefaultConfig();
+        config = new config(this);
         Bukkit.getPluginManager().registerEvents(this,this);
-        if(!getConfig().contains("//")){
-            getConfig().set("//","不执命令行可以填 \"-\" 玩家变量是 %玩家% ");
-            try {
-                getConfig().save(new File(getDataFolder(),"Config.yml"));
-            } catch (IOException e) {
-                e.printStackTrace();
-                getLogger().warning("配置文件生成失败");
-            }
-        }
-        if(!getConfig().contains("点击时后台命令")){
-            getConfig().set("点击时后台命令","say %玩家%");
-            try {
-                getConfig().save(new File(getDataFolder(),"Config.yml"));
-            } catch (IOException e) {
-                e.printStackTrace();
-                getLogger().warning("配置文件生成失败");
-            }
-        }
-        if(!getConfig().contains("点击时玩家命令")){
-            getConfig().set("点击时玩家命令","say %玩家%");
-            try {
-                getConfig().save(new File(getDataFolder(),"Config.yml"));
-            } catch (IOException e) {
-                e.printStackTrace();
-                getLogger().warning("配置文件生成失败");
-            }
-        }
         getLogger().info("加载完成");
     }
     /*
@@ -49,18 +25,37 @@ public class main extends JavaPlugin implements Listener {
     * */
     @EventHandler(priority=EventPriority.MONITOR)
     public void wanjiadianji(InventoryClickEvent a){
+        System.out.println("--------------------------------------------");
         if(a.isCancelled()){
+            System.out.println("其他插件取消了本事件");
+        }
+        System.out.println("点击的格子序号是:"+a.getRawSlot());
+        System.out.println("左键:"+a.isLeftClick());
+        System.out.println("右键:"+a.isRightClick());
+        System.out.println("shift:"+a.isShiftClick());
+        System.out.println("鼠标物品:"+a.getCursor());
+        System.out.println("点击物品:"+a.getCurrentItem());
+        System.out.println("格子类型:"+a.getSlotType());
+        System.out.println("Action类型:"+a.getAction());
+        System.out.println("Click类型:"+a.getClick());
+        System.out.println("--------------------------------------------");
+
+
+
+        if(a.getRawSlot()!=-999){
             return;
         }
-        if(a.getRawSlot()==-999){
-            if(a.getCursor().getType().equals(Material.AIR)){
-                if(!getConfig().getString("点击时后台命令").equals("-")){
-                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(),getConfig().getString("点击时后台命令").replaceAll("%玩家%",a.getWhoClicked().getName()));
-                }
-               if(!getConfig().getString("点击时玩家命令").equals("-")){
-                   org.bukkit.Bukkit.getPlayer(a.getWhoClicked().getName()).performCommand(getConfig().getString("点击时玩家命令").replaceAll("%玩家%",a.getWhoClicked().getName()));
-               }
-            }
+        if(!a.getCursor().getType().equals(Material.AIR)){
+            return;
+        }
+
+
+
+        if(!getConfig().getString("点击时后台命令").equals("-")){
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),getConfig().getString("点击时后台命令").replaceAll("%玩家%",a.getWhoClicked().getName()));
+        }
+        if(!getConfig().getString("点击时玩家命令").equals("-")){
+            org.bukkit.Bukkit.getPlayer(a.getWhoClicked().getName()).performCommand(getConfig().getString("点击时玩家命令").replaceAll("%玩家%",a.getWhoClicked().getName()));
         }
     }
 }
